1ffd34a88903a69a6b1331fda76023fc3f17d283
thanhduyluu <43946897+thanhduyluu@users.noreply.github.com>
2019-06-22 18:52:51 +0700

    Add files via upload

---
+++ trainVGG.py
@@ -0,0 +1,126 @@
+
+import numpy
+
+
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+
+from keras.optimizers import SGD
+from keras.layers import Dropout
+from keras.layers import Flatten
+
+from keras.utils import np_utils
+from keras import backend as K
+import load_data
+from keras.models import Sequential
+from keras.layers import Dense
+
+import keras
+K.set_image_dim_ordering('th')
+# fix random seed for reproducibility
+seed = 7
+numpy.random.seed(seed)
+# load data
+(X_train,y_train)=load_data.data_set
+
+
+# normalize inputs from 0-255 to 0.0-1.0
+X_train = X_train.astype('float32')
+
+#X_test = X_test.astype('float32')
+
+#X_train = X_train / 255.0
+
+#X_test = X_test / 255.0
+# one hot encode outputs
+
+y_train = np_utils.to_categorical(y_train)
+
+#y_test = np_utils.to_categorical(y_test)
+
+num_classes = y_train.shape[1]
+# Create the model
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(1,224,224)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2, 2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compile model
+epochs = 20
+lrate = 0.1
+decay = 1e-6
+sgd = SGD(lr=lrate, momentum=0.9, decay=decay, nesterov=False)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+#callbacks=[keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')]
+
+callbacks=[keras.callbacks.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=4, write_graph=True, write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)]
+
+# Fit the model
+model.fit(X_train, y_train, epochs=epochs, batch_size=4,shuffle=True,callbacks=callbacks)
+
+# Final evaluation of the model
+scores = model.evaluate(X_train, y_train, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
+# serialize model to JSONx
+model_json = model.to_json()
+with open("model_face.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model_face.h5")
+print("Saved model to disk")
+
+
+
+
+# later...
+
+# load json and create model
+#json_file = open('model_face.json', 'r')
+#loaded_model_json = json_file.read()
+#json_file.close()
+#loaded_model = model_from_json(loaded_model_json)
+# load weights into new model
+#loaded_model.load_weights("model_face.h5")
+#print("Loaded model from disk")
+
+
